e67b505e8a2305e71bc1d52d2019cc9891d00703
Kascique Lowmans <Kascique@users.noreply.github.com>
2018-09-19 14:43:18 -0400

    index.js
    main JavaScript file

---
+++ index.js
@@ -0,0 +1,11 @@
+$('*[rating] > i').click(function(e){
+   var numChild = $("*[rating] i").length+1, 
+       target = $(e.target),
+       targetNum = $(target).index(target.this)+2;
+   for(var x = 0;x<numChild;x++){
+      $('*[rating] > i:nth-child('+x+')').html("star_border");
+   }
+   for(var x = 0;x<targetNum;x++){
+      $('*[rating] > i:nth-child('+x+')').html("star");
+   }
+});
