55a4f908bccbdb97950f781636b7ab9833813aa8
Kascique Lowmans <kascique@themastercoder.com>
2019-03-23 10:44:22 -0400

    Remove unwanted brackets

--- js/index.js
+++ js/index.js
@@ -1,12 +1,12 @@
-$('*[rating] > i').click(function(e){
-   var numChild = $("*[rating] i").length+1, 
-       target = $(e.target),
-       targetNum = $(target).index(target.this)+2;
-   for(var x = 0;x<numChild;x++){
-      $('*[rating] > i:nth-child('+x+')').html("star_border");
-   }
-   for(var x = 0;x<targetNum;x++){
-      $('*[rating] > i:nth-child('+x+')').html("star");
-   }
-   $('[rating]').attr('val', targetNum-1);
+$("*[rating] > i").click(function(e) {
+  var numChild = $("*[rating] i").length + 1,
+    target = $(e.target),
+    targetNum = $(target).index(target.this) + 2;
+  for (var x = 0; x < numChild; x++)
+    $("*[rating] > i:nth-child(" + x + ")").html("star_border");
+   
+  for (var x = 0; x < targetNum; x++)
+    $("*[rating] > i:nth-child(" + x + ")").html("star");
+   
+  $("[rating]").attr("val", targetNum - 1);
 });
